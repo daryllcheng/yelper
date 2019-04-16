bbbbb00d752ef19bbf92dd998827a41f1a232ee4
Daryll <daryllcsh@gmail.com>
2019-04-16 13:27:32 -0700

    Remove unused packages

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export const RESTAURANTS_QUERY = gql`
-  query resturantsQuery($city: String, $term: String) {
+  query restaurantsQuery($city: String, $term: String) {
     search(term: $term, location: $city, categories: "Restaurants", limit: 10) {
       total
       business {
